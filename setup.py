60c6830d477f635ba030ccabe8da66dded339fbf
Nicholas Long <nicholas.long@nrel.gov>
2022-04-29 09:33:30 -0600

    remove pyscaffold

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@
 """
     Setup file for modelica_builder.
     Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 3.2.3.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
 """
 import distutils.cmd
 import distutils.log
@@ -90,6 +86,5 @@ except VersionConflict:
 
 if __name__ == "__main__":
     setup(
-        use_pyscaffold=True,
         cmdclass={"update_licenses": UpdateLicenses},
     )

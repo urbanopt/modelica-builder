8e86994847782c1d6353bac11fb61202ee33f596
Nathan Moore <nathan.moore@nrel.gov>
2023-08-25 11:32:47 -0600

    update deprecated call to pkg_resources (#65)
    * update deprecated call to pkg_resources
    
    * argh, pre-commit
    
    * import from `typing` instead of `typing.io`

--- modelica_builder/__init__.py
+++ modelica_builder/__init__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
 from modelica_builder.transformer import Transformer
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = 'modelica-builder'
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = version(dist_name)
+except PackageNotFoundError:
     __version__ = 'unknown'
 finally:
-    del get_distribution, DistributionNotFound
+    del version, PackageNotFoundError
 
 __all__ = ['Transformer',
            'Transformation',

3247cc5d691994e84792b24cb4e88c3c8acca786
Nathan Moore <nathan.moore@nrel.gov>
2024-10-25 08:00:19 -0600

    regenerate lexer etc with docker commands

--- modelica_builder/modelica_parser/modelicaLexer.py
+++ modelica_builder/modelica_parser/modelicaLexer.py
@@ -1,4 +1,4 @@
-# Generated from /var/antlrResult/modelica.g4 by ANTLR 4.13.1
+# Generated from /var/antlrResult/modelica.g4 by ANTLR 4.13.2
 from antlr4 import *
 from io import StringIO
 import sys
--- modelica_builder/modelica_parser/modelicaParser.py
+++ modelica_builder/modelica_parser/modelicaParser.py
@@ -1,4 +1,4 @@
-# Generated from /var/antlrResult/modelica.g4 by ANTLR 4.13.1
+# Generated from /var/antlrResult/modelica.g4 by ANTLR 4.13.2
 # encoding: utf-8
 from antlr4 import *
 from io import StringIO
